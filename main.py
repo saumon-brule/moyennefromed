7caaf25e5b14a44dc80b17df7ed7ea503de3779a
saumon-brule <75emilien75@gmail.com>
2022-10-26 09:31:14 +0200

    escape

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
 
 @app.route("/aaa")
 def hello_world2():
