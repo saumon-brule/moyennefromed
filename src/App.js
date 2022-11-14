cad5ae0e5dda2b7b4f0d40504f666ef3d52c34a1
saumon-brule <75emilien75@gmail.com>
2022-11-14 18:39:02 +0100

    BTC v1.4.8

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ function App() {
         console.log(datas["data"])
         console.log(datas["data"]["quotes"])
         console.log(datas["data"]["quotes"][(datas["data"]["quotes"].lenght - 1)])
-        setPriceBTCNow(Number(datas["data"]["quotes"][(datas["data"]["quotes"].lenght - 1)]["c"]))
+        setPriceBTCNow(Number(datas["data"]["quotes"][(datas["data"]["quotes"].lenght - 1)].c))
         console.log(`le prix du BTC actuellement est de ${priceBTCNow}€`)
       })
 
